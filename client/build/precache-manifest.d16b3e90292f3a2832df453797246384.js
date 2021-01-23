5c07d882b7cb350a1995bab400d7d1d13d770ceb
Diko <sandikoprastyo@gmail.com>
2021-01-24 05:13:57 +0700

    make better

---
+++ client/build/precache-manifest.d16b3e90292f3a2832df453797246384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5497f9d5fcaf1b9944837e49ab78af28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f3af6c40f3fc4370e39",
+    "url": "/static/css/2.aa59491a.chunk.css"
+  },
+  {
+    "revision": "0f3af6c40f3fc4370e39",
+    "url": "/static/js/2.1d6b5c12.chunk.js"
+  },
+  {
+    "revision": "1b9cc396521eecb2045c787139670fba",
+    "url": "/static/js/2.1d6b5c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94889be8496fec8b0bf",
+    "url": "/static/js/main.dd3e81df.chunk.js"
+  },
+  {
+    "revision": "90faf42f038ba898279a61dba8c33838",
+    "url": "/static/js/main.dd3e81df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "249db38344789eaa6fea",
+    "url": "/static/js/runtime-main.fa940d98.js"
+  }
+]);
