1b41b4993c105f382abd51275722a02e191feeaa
Diko <sandikoprastyo@gmail.com>
2021-01-24 03:51:07 +0700

    Added a Procfile.

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -9,7 +9,7 @@ router.route("/").get((req, res) => {
 
 router.route("/add").post((req, res) => {
   const username = req.body.username;
-  const newUser = new User({ username });
+  const newUser = new User({ username }); 
 
   newUser
     .save()
